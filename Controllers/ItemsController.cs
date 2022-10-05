a7888a7cf9a8ac10cbf55693a45efbd8952014e7
PincurchinFan488 <camachoj14@mail.gtc.edu>
2022-10-04 19:28:02 -0500

    Last Commit for Part 4

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -56,6 +56,44 @@ public class ItemsController : ControllerBase
 
         return CreatedAtAction(nameof(GetItem), new {id = item.Id}, item.AsDto());
     }
+
+    // Put / items/ {id}
+    [HttpPut("{id}")]
+    public ActionResult UpdateItem(Guid id, UpdateItemDto itemDto)
+    {
+        var existingItem = repository.GetItem(id);
+
+        if (existingItem is null)
+        {
+            return NotFound();
+        }
+
+        Item updatedItem = existingItem with {
+            Name = itemDto.Name,
+            Price = itemDto.Price
+            
+        };
+
+        repository.UpdateItem(updatedItem);
+
+        return NoContent();
+    }
+
+    // DELETE / items /{id}
+    [HttpDelete("{id}")]
+    public ActionResult DeleteItem(Guid id)
+    {
+         var existingItem = repository.GetItem(id);
+
+        if (existingItem is null)
+        {
+            return NotFound();
+        }
+
+        repository.DeleteItem(id);
+
+        return NoContent();
+    }
 }
 
 }
